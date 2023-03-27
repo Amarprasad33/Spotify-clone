950fdacaab1472a1d126c2b118685d0c02c9b906
Amar Prasad <amarprasad77354@gmail.com>
2023-03-27 20:07:27 +0530

    intro to context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { DataLayer } from './DataLayer';
+import { initialState } from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+    <DataLayer initialState={initialState} reducer>
+        <App />
+    </DataLayer>
+    </React.StrictMode>
 );
 
 
