0e51b9cf27cc14f52653850f9e68ed35f9826a56
Amar Prasad <amarprasad77354@gmail.com>
2023-03-28 20:42:49 +0530

    fetched data from api

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,11 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
-
+import { useStateValue } from '../StateProvider';
 
 function Sidebar() {
+    const [{playlists}, dispatch] = useStateValue();
+
     return (
         <div className='sidebar'>
             <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt='' /> 
@@ -17,6 +19,10 @@ function Sidebar() {
             <br />
             <strong className='sidebar_title'>PLAYLISTS</strong>
             <hr />
+
+            {playlists?.items?.map(playlist => (
+                <SidebarOption key={playlist.name} title={playlist.name} />
+            ))}
 
             <SidebarOption title="Hip hop" />
             <SidebarOption title="Rock" />
