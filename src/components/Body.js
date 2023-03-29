860936fc7f3e4354aebe9af4d51e89f5d89d368a
Amar Prasad <amarprasad77354@gmail.com>
2023-03-29 11:19:36 +0530

    making the body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,11 @@
 import React from "react";
 import '../styles/Body.css';
+import Header from "./Header"; 
 
-
-function Body() {
+function Body({ spotify }) {
     return (
         <div className="body">
-            <h1>I am the body</h1>
+            <Header spotify={spotify}/>
         </div>
     );
 }
