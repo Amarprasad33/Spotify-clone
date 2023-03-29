d0538b269d25306fa55833809a66d2863432eb22
Amar Prasad <amarprasad77354@gmail.com>
2023-03-29 10:45:54 +0530

    updated api calls

--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,8 @@ export const initialState = {
     playing: false,
     item: null,
     // REMOVE after finished developing
-    // token: 
-    //     "BQAIS3wQusr0DBRmWhE1tG2YoiuDiaSMTBFaabRtf_rOLN9Mb8sb_nMPniT7grUKk5p4hI19lbm96ZSpWwrrNuABoUgiQrtEjCCr9y0Z2lrmVh6yC_OpRykR5ZNVTZrygpLpyjrMBJocCkQFPOesLO69VlgD4pkNziG5ID2Wh_ZfZyNo3rCIGC1WrLUrQ_O2cRTI_gDxKS0mrx_lHz60cA"
+    token: 
+        "BQAIS3wQusr0DBRmWhE1tG2YoiuDiaSMTBFaabRtf_rOLN9Mb8sb_nMPniT7grUKk5p4hI19lbm96ZSpWwrrNuABoUgiQrtEjCCr9y0Z2lrmVh6yC_OpRykR5ZNVTZrygpLpyjrMBJocCkQFPOesLO69VlgD4pkNziG5ID2Wh_ZfZyNo3rCIGC1WrLUrQ_O2cRTI_gDxKS0mrx_lHz60cA"
 }
 
 const reducer = (state, action) => {
